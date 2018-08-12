102e629d16b5d7bce24df1171f48305abe209692
rakesh <rakesh.webtest@gmail.com>
2018-08-12 23:39:57 +0530

    recipe value change

--- src/app/NutrientsInMyFood/app.recipe.component.ts
+++ src/app/NutrientsInMyFood/app.recipe.component.ts
@@ -5,15 +5,14 @@ import { KnowRecipeValuesComponent } from './app.knowrecipeValues.component';
 // import { DBService } from '../Services/dbservice'
 // import { EventService } from '../Services/eventservice';
 import { Storage } from '@ionic/storage';
+import { DBService } from '../Services/dbservice';
+import { EventService } from '../Services/eventservice';
 declare var window;
 
 
 @Component({
   selector: 'recipe',
   templateUrl: './recipe.html',
-  styles: [`
-
-  `]
 })
 export class RecipeComponent {
   devHeight;
@@ -25,17 +24,37 @@ export class RecipeComponent {
   pageTitle = "Recipe";
   currentitem: any = [];
   // storage = new Storage();
-  constructor(private navController: NavController, private storage:Storage) {
+  constructor(private navController: NavController, 
+    private eventservice: EventService,
+    private storage:Storage,private dbservice:DBService) {
 
   }
 
 
 
   ngOnInit() {
+    let getRecipiesQuery = "SELECT uid_recipes, Itemname, one_serving_wt_g FROM recipes";
 
+    this.dbservice.getDataFromTable('getrecipies', 'recipes.sql', getRecipiesQuery, function (a, b) {
+      // var c = Object.values(b);
+      // const data = b;
+      console.log('b',b);
+     
+    });
+    this.eventservice.getMessage().subscribe((data) => {
+
+      if (data.name == 'getrecipies') {
+
+        this.storage.set('totalRecipies', Array.from(data.value.values));
+        // alert(JSON.stringify(data.value.values))
+         this.totalRecipies = Array.from(data.value.values);
+
+      }
+
+    });
     this.storage.get('totalRecipies').then((data1) => {
       // alert(data1);
-      this.totalRecipies = data1;
+     // this.totalRecipies = data1;
 
     });
 
@@ -61,7 +80,7 @@ export class RecipeComponent {
 
     if (filterStr.length > 0) {
       this.filteredRecipies = this.totalRecipies.filter((item) => {
-        return (item[0].toLowerCase().includes(filterStr.toLowerCase())) 
+        return (item[1].toLowerCase().includes(filterStr.toLowerCase())) 
       });
       this.isDisplayRecipeList = true;
       // alert(JSON.stringify(this.filteredRecipies));
